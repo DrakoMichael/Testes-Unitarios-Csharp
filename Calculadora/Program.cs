ff066e42813483dd2a4ddd317b8701be74195a37
Michael Mello <michaelkcms@gmail.com>
2023-10-22 17:48:15 -0300

    Atualização

---
+++ Calculadora/Program.cs
@@ -0,0 +1,7 @@
+﻿using Calculadora.Services;
+
+CalculadoraImp c = new CalculadoraImp();
+
+int num1 = 5;
+int num2 = 10;
+Console.WriteLine($"a soma entre {num1} e {num2} é: {c.Somar(num1, num2)}"); 
